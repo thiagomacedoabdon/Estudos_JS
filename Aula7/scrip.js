60e75e06140ce2439870c722a5c0ea20411aac3f
Thiago Macedo <111608892+thiagomacedoabdon@users.noreply.github.com>
2023-08-24 06:43:12 -0300

    Create scrip.js

---
+++ Aula7/scrip.js
@@ -0,0 +1,44 @@
+//adição
+let a = 10
+let b = 5
+console.log(a+b)
+let c = a+b //pode ser assim
+console.log(c)
+
+//subtração
+let d = 8
+let e = 2
+console.log(d-e)
+let f = d-e //pode ser assim
+console.log(f)
+
+//multplicação
+let g = 4
+let h = 10
+console.log(g*h)
+let i = g*h //pode ser assim
+console.log(i)
+
+//divisão
+let j = 20
+let k = 5
+console.log(j/k)
+let l = j/k //pode ser assim
+console.log(l)
+
+//incremento
+let m = 10
+m++
+console.log(m)
+
+//decremento
+let n = 10
+n--
+console.log(n)
+
+//atribuição
+let o = 40
+o += 20
+console.log(o)
+o -= 10
+console.log(o)
