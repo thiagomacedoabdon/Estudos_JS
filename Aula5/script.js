2a4ff6560af78d3c2f5bff9fe4f12860b8bfd7f0
Thiago Macedo <111608892+thiagomacedoabdon@users.noreply.github.com>
2023-08-24 06:41:31 -0300

    Create script.js

---
+++ Aula5/script.js
@@ -0,0 +1,16 @@
+ 
+ // O VAR não é mais tão utilizado
+ var nome = 'Thiago'
+ var idade = 36
+ console.log(nome, idade)
+
+
+ let nome1 = 'Thiago'
+ let idade1 = 36
+ nome1 = 'Fulano'
+ console.log(nome1, idade1)
+
+ //A variável no CONST não pode ser alterada. É uma CONSTANTE.
+ const nome2 = 'Thiago'
+ const idade2 = 36
+ console.log(nome2, idade2)
