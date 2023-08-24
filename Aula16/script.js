7042de539b1c93fd1e449844fc8198461849e06d
Thiago Macedo <111608892+thiagomacedoabdon@users.noreply.github.com>
2023-08-24 06:49:10 -0300

    Create script.js

---
+++ Aula16/script.js
@@ -0,0 +1,11 @@
+
+
+function soma(num1, num2) {
+    
+    return num1 + num2
+}
+
+let resultado = soma(10, 5)
+
+console.log(resultado) //exibe no console
+document.write(resultado) //exibe na tela
